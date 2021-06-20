8ee50d5d1d4f568ff412c4d0322fa85473d4ff63
nguyenle <nguyenle@localhost>
2021-06-20 10:43:04 +0200

    Queue Implemented

--- data_structures/src/queue/Queue.java
+++ data_structures/src/queue/Queue.java
@@ -1,5 +1,24 @@
 package queue;
 
 public class Queue {
-
+	String[] items = new String[100];
+	int head = 0;
+	int tail = 0;
+	
+	void enqueue(String item) {
+		items[tail] = item;
+		tail += 1;
+		if(tail == items.length) tail = 0;
+	}
+	
+	String dequeue() {
+		String value = items[head];
+		head += 1;
+		if (head == items.length) head = 0;
+		return value;
+	}
+	
+	boolean isEmpty() {
+		return head == tail;
+	}
 }
