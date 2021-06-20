3bbbb3e6c8d42ffae45f78fe2a8d4d8e54d4e1e2
nguyenle <nguyenle@localhost>
2021-06-20 10:35:37 +0200

    Stack implemented

--- data_structures/src/stacks/Main.java
+++ data_structures/src/stacks/Main.java
@@ -1,5 +1,17 @@
 package stacks;
 
-public class Main {
-
+public class Main{
+	public static void main(String[] args){
+		//creating a stack
+		Stack testStack = new Stack();
+		
+		//testing push function
+		testStack.push("hello");
+		testStack.push("how");
+		testStack.push("are");
+		testStack.push("you");
+		
+		//testing pop function
+		System.out.println(testStack.pop());
+	}
 }
