d06e5db21657e13293821cf9d9ec359d079906eb
nguyenle <nguyenle@localhost>
2021-06-20 10:35:50 +0200

    Stack implemented

--- data_structures/src/stacks/Stack.java
+++ data_structures/src/stacks/Stack.java
@@ -1,5 +1,26 @@
 package stacks;
 
-public class Stack {
-
+public class Stack{
+	String[] array = new String[100];
+	int size = 0;
+	
+	void push(String inputData){
+		array[size] = inputData;
+		size+= 1;
+	}
+	
+	String pop(){
+		size -= 1;
+		return array[size];
+	}
+	
+	String isStackEmpty(){
+		if(size == 0){
+			return "Stack is empty";
+		}
+		else{
+			return "Stack is not empty";
+		}
+	}
 }
+
