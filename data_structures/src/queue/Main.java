4a848cc5099a9ede0621add0d6b6f357bf7e5bea
nguyenle <nguyenle@localhost>
2021-06-20 10:42:56 +0200

    Queue Implemented

--- data_structures/src/queue/Main.java
+++ data_structures/src/queue/Main.java
@@ -1,5 +1,26 @@
 package queue;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		//creating a queue
+		Queue testQueue = new Queue();
+		
+		for (int i = 0; i < 110; i += 1) {
+			testQueue.enqueue("hello");
+			testQueue.dequeue();
+		}
+		
+		Scanner scan = new Scanner(System.in);
+		String[] words = scan.nextLine().split(" ");
+		
+		for (String word : words) {
+			testQueue.enqueue(word);
+		}
+		
+		while(testQueue.isEmpty() == false) {
+			System.out.println(testQueue.dequeue());
+		}
+	}
 }
